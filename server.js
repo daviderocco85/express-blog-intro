76edbe1e1eb322b750c208cb997cbc21b85697ba
daviderocco85 <roccodavide85@gmail.com>
2026-08-31 17:14:33 +0200

    feat: add bacheca route and import posts array in server.js

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 import express from 'express';
+import { posts } from './data/posts.js';
 const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
     res.send('Server del mio blog');
+});
+app.get('/bacheca', (req, res) => {
+    res.json(posts);
 });
 
 app.listen(port, () => {
